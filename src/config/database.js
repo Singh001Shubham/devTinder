1d5abd3e1121fe51ac61b6a9211721c017c8e55c
Shubham <you@example.com>
2025-10-16 03:11:46 +0530

    update password changed

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,5 @@
 const { mongoose } = require("mongoose");
-const URI = "mongodb://localhost:27017/";
-
+const URI = "mongodb+srv://shubhamsingh9933_db_user:Sq65iuvp395OGiNx@devtinder.uxxasmo.mongodb.net/";
 async function dbConnect(){
     await mongoose.connect(URI);
 }
